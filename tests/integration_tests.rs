b09d86c10f1cf71c5fbcacc1c19a85c3ceb17d95
Abhishek Roy <abhishek.roy@perspectum.com>
2023-10-27 21:15:07 +0100

    add integration test

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,1 +1,22 @@
-extern crate sudoku;
+extern crate sudoku_solver;
+use std::path::PathBuf;
+use sudoku_solver::board::{solve_mut, Board};
+#[test]
+fn test_pass() {
+    let mut board = Board::from_csv(&PathBuf::from("tests/test_board_pass.csv")).unwrap();
+    solve_mut(&mut board);
+    assert_eq!(
+        board,
+        Board::new(&[
+            [9, 7, 3, 8, 5, 4, 2, 1, 6],
+            [1, 4, 6, 2, 7, 9, 5, 8, 3],
+            [2, 8, 5, 6, 1, 3, 4, 9, 7],
+            [8, 1, 2, 3, 9, 5, 6, 7, 4],
+            [7, 5, 9, 4, 8, 6, 3, 2, 1],
+            [6, 3, 4, 1, 2, 7, 9, 5, 8],
+            [3, 9, 7, 5, 6, 1, 8, 4, 2],
+            [5, 6, 8, 7, 4, 2, 1, 3, 9],
+            [4, 2, 1, 9, 3, 8, 7, 6, 5]
+        ])
+    )
+}
